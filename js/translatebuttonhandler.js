fe3b164774d3178be2349a542ad3f341c80335fe
Devan Huapaya <huapayadevan@gmail.com>
2015-08-15 14:22:10 -0700

    changes to front end

--- js/translatebuttonhandler.js
+++ js/translatebuttonhandler.js
@@ -17,12 +17,15 @@ function loadXMLDoc()
       {
       //  document.getElementById("myDiv").innerHTML=xmlhttp;
         data = document.getElementById("input-section--message-content").value;
-
-        console.log(JSON.parse(xmlhttp.response).responseData.translatedText);
+        $("#translation-section--message-content").html(JSON.parse(xmlhttp.response).responseData.translatedText);
       }
   }
 var url = "http://api.mymemory.translated.net/get?q=" + data + "&langpair=en|es";
   xmlhttp.open("GET",url,true);
   xmlhttp.send();
 
+}
+
+function tAndSButtonPressed(){
+  loadXMLDoc();
 }

ec8e031ce0fe955db4d27f7d6bbc18988dff57d5
Devan Huapaya <huapayadevan@gmail.com>
2015-08-15 17:28:53 -0700

    ∆ javascript button handles

--- js/translatebuttonhandler.js
+++ js/translatebuttonhandler.js
@@ -1,28 +1,37 @@
-
-console.log('called');
 var tb, tBox = {
     vars: {
         isOpen: false,
         domObj: $("#translation-section-container"),
-        translateButton: $("#translate-button")
+        translateButton: $("#translate-button"),
+        cancelButton: $("#translate-cancel-button")
     },
     init: function () {
         tb = this.vars;
         this.bindUIElements();
-        console.log('called');
     },
     bindUIElements: function () {
-        console.log('called');
         tb.translateButton.on('click', function () {
-          console.log('called');
-          if (tb.isOpen) {
-              tb.domObj.removeClass("open");
-              tb.isOpen = false;
-          } else {
-              tb.domObj.addClass("open");
-              tb.isOpen = true;
-          }
+            if (tb.isOpen) {
+                tb.domObj.removeClass("open");
+                tb.isOpen = false;
+            } else {
+                tb.domObj.addClass("open");
+                tb.isOpen = true;
+            }
         });
+
+        tb.cancelButton.on('click', function () {
+            tb.domObj.removeClass("open");
+            tb.isOpen = false;
+        });
+
+        $(document).keyup(function (e) {
+            if (e.keyCode == 27 && tb.isOpen) {
+                tb.domObj.removeClass("open");
+                tb.isOpen = false;
+            }
+        });
+
     }
 };
 

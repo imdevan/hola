9d025371fc24f7b45d42ed08fcc9eb58fe1120a1
vatyx <sahildhanju1@gmail.com>
2015-08-16 03:32:17 +0000

    Added parse functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+Parse.initialize("rYhgbAiMXfzb692ANgZXoUVGQkhUOuMSnCY2sHtk", "yTfmNgzwyG2B7OirvNrQG0MqbsNrt9sxEvLyE3Yw");
+					
+var TwilioNumber = "+19152350594";
+var OtherNumber = "+12144035793";
+
 var socket;
 
 socket = io();
@@ -17,6 +22,10 @@ socket.on('connect',function() {
 socket.on('message',function(data) {
 		//Display data on screen
 		//Do something with data
+	var MessageObject = Parse.Object.extend("Message");
+    var messageObject = new MessageObject();
+    messageObject.save({sendingNumber: OtherNumber, BodyNotTranslated: data, BodyTranslated: data});
+		
 	console.log('Received a message from the server!',data);
 });
 	// Add a disconnect listener
@@ -33,3 +42,20 @@ function sendMessageToServer(num, msg) {
 function sendPhoneNumberToServer(msg) {
 	socket.emit('phoneNumber', msg);
 }
+
+var query = new Parse.Query("Message");
+query.ascending("createdAt");
+query.find({
+  success: function(results) {
+    console.log(results.length);
+    for(var i = 0; i < results.length; i++)
+    {
+    	console.log(results[i].createdAt);
+    }
+  },
+
+  error: function(error) {
+    console.log("What the hell just happened!");
+  }
+});
+
--- js/translatebuttonhandler.js
+++ js/translatebuttonhandler.js
@@ -1,14 +1,30 @@
 function load(){
   function responseText () {
-    // console.log(this.responseText);
-   console.log(JSON.parse(this.responseText).responseData.translatedText);
-   sendMessageToServer('+12144035793', JSON.parse(this.response).responseData.translatedText);
+   var translated = JSON.parse(this.responseText).responseData.translatedText;
+   console.log(translated);
+   
+    var MessageObject = Parse.Object.extend("Message");
+    var messageObject = new MessageObject();
+    messageObject.save({sendingNumber: TwilioNumber, BodyNotTranslated: data, BodyTranslated: translated});
+   
+   sendMessageToServer('+12144035793', translated); // after translation
   }
-  var data = document.getElementById("input-section--message-content").value;
+  var data = document.getElementById("input-section--message-content").value; // this will be the text before translation
   var url = "https://api.mymemory.translated.net/get?q=" + data + "&langpair=es|en";
-
+ 
   var oReq = new XMLHttpRequest();
   oReq.addEventListener('load', responseText);
   oReq.open("get", url, true);
   oReq.send();
 }
+
+// { Phonenumber : nunmber, origionaltext : data, translatedtext: data}
+
+// {phonenumber: number,
+//   conversation:{
+//     [message:{
+//       sent/received
+      
+//     }]
+    
+//   }
